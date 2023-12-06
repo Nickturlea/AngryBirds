8a58ad1f74abfb23052088bf34065d94354788c4
Nicholas Turlea <nickturlea@gmail.com>
2023-12-06 14:49:34 -0500

    Added Bird Component (not finctional yet)

---
+++ AngryBirds/BirdComponent.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct3D9;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngryBirds
+{
+    internal class BirdComponent : DrawableGameComponent
+    {
+        private SpriteBatch spriteBatch;
+        private Texture2D birdTexture;
+        private Vector2 position;
+        private int imageWidth;
+        private int imageHeight;
+
+            public BirdComponent(Game game, Vector2 initialPosition, Texture2D birdTexture, int width, int height) : base(game)
+            {
+                this.spriteBatch = new SpriteBatch(game.GraphicsDevice);
+                this.position = initialPosition;
+                this.birdTexture = birdTexture;
+                this.imageWidth = width;
+                this.imageHeight = height;
+            }
+
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(birdTexture, new Rectangle((int)position.X, (int)position.Y, imageWidth, imageHeight), Color.White);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+    }
+}
--- AngryBirds/PlayScene.cs
+++ AngryBirds/PlayScene.cs
@@ -6,14 +6,18 @@ internal class PlayScene : GameScene
 {
     private SpriteBatch sb;
     private SlingShotComponent slingShot;
+    private BirdComponent bird;
 
     public PlayScene(Game game) : base(game)
     {
         Game1 g = (Game1)game;
         this.sb = g._spriteBatch;
         Texture2D slingShotTexture = Game.Content.Load<Texture2D>("Images/SlingShot");
-        slingShot = new SlingShotComponent(game, new Vector2(100, 200), slingShotTexture, 100, 150); 
+        Texture2D birdTexture = Game.Content.Load<Texture2D>("Images/Bird");
+        slingShot = new SlingShotComponent(game, new Vector2(100, 200), slingShotTexture, 100, 150);
+        bird = new BirdComponent(game, new Vector2(200, 100), slingShotTexture, 50, 50);
         Components.Add(slingShot);
+        Components.Add(bird);
     }
 
     public override void Draw(GameTime gameTime)
@@ -21,6 +25,7 @@ internal class PlayScene : GameScene
         sb.Begin();
 
         slingShot.Draw(gameTime);
+        bird.Draw(gameTime);
 
         sb.End();
 
