c631f9d356eef0e80699777c1e30090a9ccd5765
Samuel Est <eestrada9744@conestogac.on.ca>
2023-12-08 00:46:23 -0500

    Added a yellow bird component

--- AngryBirds/PlayScene.cs
+++ AngryBirds/PlayScene.cs
@@ -10,6 +10,8 @@ internal class PlayScene : GameScene
     private BirdComponent bird;
     private BoxComponent brownBox;
     private BarrelComponent barrel;
+    private PigComponent pig;
+    private YellowBirdComponent yellowBird;
 
 
     public PlayScene(Game game) : base(game)
@@ -23,6 +25,8 @@ internal class PlayScene : GameScene
         Texture2D birdTexture = Game.Content.Load<Texture2D>("Images/BirdSprite");
         Texture2D boxTexture = Game.Content.Load<Texture2D>("Images/brownBox");
         Texture2D barrelTexture = Game.Content.Load<Texture2D>("Images/barrel");
+        Texture2D pigTexure= Game.Content.Load<Texture2D>("Images/pig");
+        Texture2D yellowBirdTexure = Game.Content.Load<Texture2D>("Images/yellowBird");
 
 
 
@@ -33,7 +37,7 @@ internal class PlayScene : GameScene
 
         // Position the bird on the slingshot
         Vector2 birdPosition = new Vector2(slingShotPosition.X + 67, slingShotPosition.Y -5); // Perfact measurements for starting the bird on the slingshot, nick
-        bird = new BirdComponent(game, birdPosition, birdTexture, 200, 100);
+        bird = new BirdComponent(game, birdPosition, birdTexture, 105, 105);
 
 
         // Position the Box on the slingshot
@@ -41,21 +45,37 @@ internal class PlayScene : GameScene
         brownBox = new BoxComponent(game, boxPosition, boxTexture, 200, 200);
 
         // Position the Box on the slingshot
-        Vector2 barrelPosition = new Vector2(boxPosition.X + 20, boxPosition.Y - 90); // Perfact measurements for starting the bird on the slingshot, nick
-        barrel = new BarrelComponent(game, barrelPosition, barrelTexture, 200, 200);
+        Vector2 barrelPosition = new Vector2(boxPosition.X + 55, boxPosition.Y - 350); // Perfact measurements for starting the bird on the slingshot, nick
+        barrel = new BarrelComponent(game, barrelPosition, barrelTexture, 150, 120);
+
+        // Position the Box on the slingshot
+        Vector2 pigPosition = new Vector2(birdPosition.X + 1015, barrelPosition.Y - 77); // Perfact measurements for starting the bird on the slingshot, nick
+        pig = new PigComponent(game, pigPosition, pigTexure, 80, 80);
+
+        // Position the Box on the slingshot
+        Vector2 yellowBPosition = new Vector2(boxPosition.X + 5, barrelPosition.Y + 235); // Perfact measurements for starting the bird on the slingshot, nick
+        yellowBird = new YellowBirdComponent(game, yellowBPosition, yellowBirdTexure, 155, 130);
+
+
+
 
 
         // Addinng componets 
+        Components.Add(yellowBird);
+        Components.Add(pig);
         Components.Add(brownBox);
         Components.Add(barrel);
-        Components.Add(slingShot);
+        Components.Add(slingShot); 
         Components.Add(bird);
+
     }
 
     public override void Draw(GameTime gameTime)
     {
         sb.Begin();
         sb.Draw(currBackGround, new Rectangle(0, 0, (int)Shared.stage.X, (int)Shared.stage.Y), Color.White);
+        yellowBird.Draw(gameTime);
+        pig.Draw(gameTime);
         barrel.Draw(gameTime);
         slingShot.Draw(gameTime);
         bird.Draw(gameTime);
